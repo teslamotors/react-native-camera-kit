cdbf91333fb3230164faa6ad5bc516ab65ea9702
Ran Greenberg <rang@wix.com>
2017-03-13 12:30:55 +0200

    update index files

--- index.ios.js
+++ index.ios.js
@@ -1,11 +1,13 @@
 import CameraKitGallery from './src/CameraKitGallery';
 import CameraKitCamera from './src/CameraKitCamera';
 import CameraKitGalleryView from './src/CameraKitGalleryView';
+import CameraKitCameraScreen from './src/CameraKitCameraScreen';
 
 export {
 	CameraKitGallery,
 	CameraKitCamera,
-  CameraKitGalleryView
+  CameraKitGalleryView,
+	CameraKitCameraScreen
 };
 
 
