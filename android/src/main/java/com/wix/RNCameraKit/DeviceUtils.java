fa500703182494bfcd668ad11cd724a050e9ae71
Guy Carmeli <guyc@wix.com>
2017-08-22 17:19:40 +0300

    Fix isGoogleDevice method
    Originally, we thought the upside down selfie photos issue was
    exclusive to Nexus 6 and Pixel devices. Apparently it's reproducible on
    Nexus 5 as well.

--- android/src/main/java/com/wix/RNCameraKit/DeviceUtils.java
+++ android/src/main/java/com/wix/RNCameraKit/DeviceUtils.java
@@ -4,6 +4,7 @@ import android.os.Build;
 
 public class DeviceUtils {
     public static boolean isGoogleDevice() {
-        return Build.MANUFACTURER.toLowerCase().contains("google") && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M;
+        return (Build.MANUFACTURER.toLowerCase().contains("google") && Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) ||
+               (Build.MANUFACTURER.toLowerCase().contains("lge") && Build.BRAND.toLowerCase().equals("google"));
     }
 }
