9b7651ee1b7c2b4a7be06c1c77bad3e1f887b6de
Roman Kozlov <romanko@wix.com>
2018-01-26 11:18:27 +0200

    index update

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import CameraKitGallery from './src/CameraKitGallery';
 import CameraKitCamera from './src/CameraKitCamera';
 import CameraKitGalleryView from './src/CameraKitGalleryView';
 import CameraKitCameraScreen from './src/CameraScreen/CameraKitCameraScreen';
+import CameraKitBarcode from './src/Barcode/CameraKitBarcode';
 
 export {
 	CameraKitGallery,
 	CameraKitCamera,
   CameraKitGalleryView,
-	CameraKitCameraScreen
+  CameraKitCameraScreen,
+  CameraKitBarcode
 };
 
 
