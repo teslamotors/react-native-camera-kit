c707967916d524012cd08e3e1351b3ca37f1ad4a
Dmytro Ponomarenko <dmytropo@wix.com>
2018-03-01 13:12:08 +0200

    edit from node_modules

--- example/src/BarcodeScreen.js
+++ example/src/BarcodeScreen.js
@@ -34,7 +34,13 @@ export default class CameraScreen extends Component {
         scanBarcode={true}
         laserColor={"blue"}
         frameColor={"yellow"}
+       
         onReadCode={((event) => console.log('QRCODE', event.nativeEvent.qrcodeStringValue))}
+        hideControls={true} 
+        isShowFrameForScanner={true}   
+        // offsetForScannerFrame = {10}  
+        // heightForScannerFrame = {300}  
+        // colorForScannerFrame = {'red'} 
       />
     );
   }
