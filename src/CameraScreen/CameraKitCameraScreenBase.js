8b60c46d6b5aec20f67b176cdfc4b49a9b37c10d
Chris Teague <chris@cteague.com.au>
2018-09-10 22:30:44 +0930

    Fixes “Undefined is not an object (evaluating '_reactNative.Image.resizeMode.contain’)” issue introduced into CameraKitCameraScreenBase with React Native 0.56
    https://github.com/facebook/react-native/commit/870775ee738e9405c6545500f9a637df9b513a02

--- src/CameraScreen/CameraKitCameraScreenBase.js
+++ src/CameraScreen/CameraKitCameraScreenBase.js
@@ -119,7 +119,7 @@ export default class CameraScreenBase extends Component {
         <Image
           style={{ flex: 1, justifyContent: 'center' }}
           source={this.state.flashData.image}
-          resizeMode={Image.resizeMode.contain}
+          resizeMode={"contain"}
         />
       </TouchableOpacity>
   }
@@ -130,7 +130,7 @@ export default class CameraScreenBase extends Component {
         <Image
           style={{ flex: 1, justifyContent: 'center' }}
           source={this.props.cameraFlipImage}
-          resizeMode={Image.resizeMode.contain}
+          resizeMode={"contain"}
         />
       </TouchableOpacity>
   }
