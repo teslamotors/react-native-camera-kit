718851765fc70bd0e1e4426f9357b830292eddbf
Shalom Yerushalmy <yershalom@gmail.com>
2018-10-17 16:35:53 +0300

    Change kotlin file to Java

---
+++ android/src/main/java/com/wix/RNCameraKit/torch/TorchModule.java
@@ -0,0 +1,64 @@
+package com.wix.RNCameraKit.torch;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
+import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+public class TorchModule extends ReactContextBaseJavaModule {
+    private final ReactApplicationContext myReactContext;
+    private Boolean isTorchOn = false;
+    private Camera camera;
+
+    public TorchModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+
+        // Need access to reactContext to check for camera
+        this.myReactContext = reactContext;
+    }
+
+    @Override
+    public String getName() {
+        return "CKTorch";
+    }
+
+    @ReactMethod
+    public void switchState(Boolean newState, Callback successCallback, Callback failureCallback) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            CameraManager cameraManager = (CameraManager) this.myReactContext.getSystemService(Context.CAMERA_SERVICE);
+
+            try {
+                String cameraId = cameraManager.getCameraIdList()[0];
+                cameraManager.setTorchMode(cameraId, newState);
+                successCallback.invoke(true);
+            } catch (CameraAccessException e) {
+                String errorMessage = e.getMessage();
+                failureCallback.invoke("Error: " + errorMessage);
+            }
+        } else {
+            Camera.Parameters params;
+
+            if (!isTorchOn) {
+                camera = Camera.open();
+                params = camera.getParameters();
+                params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+                camera.setParameters(params);
+                camera.startPreview();
+                isTorchOn = true;
+            } else {
+                params = camera.getParameters();
+                params.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+
+                camera.setParameters(params);
+                camera.stopPreview();
+                camera.release();
+                isTorchOn = false;
+            }
+        }
+    }
+}
