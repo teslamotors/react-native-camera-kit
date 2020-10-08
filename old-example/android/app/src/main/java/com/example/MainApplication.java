fffa7324807a3d6e09b5b4e0823fd84d0ba89206
Ran Greenberg <gran33@gmail.com>
2020-10-08 10:43:45 +0300

    remove in old example

--- old-example/android/app/src/main/java/com/example/MainApplication.java
+++ old-example/android/app/src/main/java/com/example/MainApplication.java
@@ -6,7 +6,7 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
-import com.wix.RNCameraKit.RNCameraKitPackage;
+import com.rncamerakit.RNCameraKitPackage;
 
 import java.util.Arrays;
 import java.util.List;
