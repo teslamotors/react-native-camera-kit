7adb4f91865ee87e65779742248375b6ca5a2c5c
aarongrider <aaron.grider@gmail.com>
2020-10-14 17:48:24 -0700

    Add back missing exports

--- src/index.tsx
+++
@@ -1,9 +0,0 @@
-import { NativeModules } from 'react-native';
-
-type CameraKitType = {
-  multiply(a: number, b: number): Promise<number>;
-};
-
-const { CameraKit } = NativeModules;
-
-export default CameraKit as CameraKitType;
