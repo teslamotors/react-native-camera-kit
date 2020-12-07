86739ddfeb4039c6051482187fc52a9dae88f17d
aarongrider <aaron.grider@gmail.com>
2020-12-06 22:45:47 -0800

    Fix formatting in CameraExample

--- example/src/CameraExample.tsx
+++ example/src/CameraExample.tsx
@@ -27,9 +27,7 @@ export default class CameraExample extends Component {
           laserColor="red" // Barcode only, optional
           frameColor="yellow" // Barcode only, optional
           surfaceColor="blue" // Barcode only, optional
-          onReadCode={(  // optional
-            event,
-          ) => console.log(event.nativeEvent.codeStringValue)}
+          onReadCode={(event) => console.log(event.nativeEvent.codeStringValue)}
         />
       </View>
     );
