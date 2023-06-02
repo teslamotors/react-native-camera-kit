05c8f1862c6b27439340a83434ad4ab0331ce904
David Bertet <david.bertet@gmail.com>
2023-06-02 02:07:18 -0700

    Improve type definition

--- src/Camera.d.ts
+++ src/Camera.d.ts
@@ -1,2 +1,41 @@
-import Camera from './Camera';
+import { CameraApi, FlashMode, FocusMode, ZoomMode, TorchMode, CameraType } from './types';
+import { Orientation } from './index';
+
+export type OnReadCodeData = {
+  nativeEvent: {
+    codeStringValue: string;
+  };
+};
+
+export type OnOrientationChangeData = {
+  nativeEvent: {
+    orientation: Orientation;
+  };
+};
+
+export interface CameraProps {
+  ref?: LegacyRef<Component<CameraApi, {}, any>>;
+  style?: StyleProp<ViewStyle>;
+  // Behavior
+  flashMode?: FlashMode;
+  focusMode?: FocusMode;
+  zoomMode?: ZoomMode;
+  torchMode?: TorchMode;
+  cameraType?: CameraType;
+  onOrientationChange?: (event: OnOrientationChangeData) => void;
+  // Barcode only
+  scanBarcode?: boolean;
+  showFrame?: boolean;
+  laserColor?: number | string;
+  frameColor?: number | string;
+  onReadCode?: (event: OnReadCodeData) => void;
+  // Specific to iOS
+  ratioOverlay?: string;
+  ratioOverlayColor?: number | string;
+  resetFocusTimeout?: number;
+  resetFocusWhenMotionDetected?: boolean;
+}
+
+declare const Camera: React.FC<CameraProps>;
+
 export default Camera;
