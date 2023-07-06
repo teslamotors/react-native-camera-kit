b0048a0bb66a26791bb8779526b85b2849bfdb26
David Bertet <david.bertet@gmail.com>
2023-07-06 00:43:39 -0700

    Remove `setTorchMode` method on Android
    `torchMode` props should be used instead

--- src/Camera.android.tsx
+++ src/Camera.android.tsx
@@ -16,9 +16,6 @@ const Camera = React.forwardRef((props: CameraProps, ref) => {
       // we must use the general module and tell it what View it's supposed to be using
       return await RNCameraKitModule.capture(options, findNodeHandle(nativeRef.current ?? null));
     },
-    setTorchMode: (mode = 'off') => {
-      RNCameraKitModule.setTorchMode(mode, findNodeHandle(nativeRef.current ?? null));
-    },
     requestDeviceCameraAuthorization: () => {
       throw new Error('Not implemented');
     },
