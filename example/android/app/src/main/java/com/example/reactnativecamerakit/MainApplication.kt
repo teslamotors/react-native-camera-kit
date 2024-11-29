91f943a514c34d0e45f95e47782b7af7059ac7bc
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2024-11-29 12:01:27 +0100

    fix: remove old way of autolinking to make codegen work on ios

--- example/android/app/src/main/java/com/example/reactnativecamerakit/MainApplication.kt
+++ example/android/app/src/main/java/com/example/reactnativecamerakit/MainApplication.kt
@@ -22,7 +22,6 @@ class MainApplication : Application(), ReactApplication {
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
               // add(MyReactNativePackage())
-              add(RNCameraKitPackage());
             }
 
         override fun getJSMainModuleName(): String = "index"
