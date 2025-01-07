d42580f38bcc33962fff3a57a8f1b8b969e73413
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2025-01-07 14:41:57 +0100

    fix: apply review suggestions

--- example/.prettierrc.js
+++ example/.prettierrc.js
@@ -1,7 +1,13 @@
 module.exports = {
-  arrowParens: 'avoid',
+  arrowParens: 'always',
   bracketSameLine: true,
-  bracketSpacing: false,
+  tabs: false,
+  tabWidth: 2,
+  bracketSpacing: true,
   singleQuote: true,
   trailingComma: 'all',
+  printWidth: 120,
+  semi: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
 };
