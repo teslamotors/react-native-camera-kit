3f4cfdd1d4d58d143be5c66d4e8951c65ed7ba0b
Aravind 🇮🇳 <36630532+aravind3566@users.noreply.github.com>
2025-02-10 16:15:18 +0700

    Update QRCodeAnalyzer.kt

--- android/src/main/java/com/rncamerakit/QRCodeAnalyzer.kt
+++ android/src/main/java/com/rncamerakit/QRCodeAnalyzer.kt
@@ -5,6 +5,7 @@ import androidx.camera.core.ExperimentalGetImage
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
 import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
 
@@ -15,8 +16,10 @@ class QRCodeAnalyzer (
     @ExperimentalGetImage
     override fun analyze(image: ImageProxy) {
         val inputImage = InputImage.fromMediaImage(image.image!!, image.imageInfo.rotationDegrees)
+        
+        val options = BarcodeScannerOptions.Builder().setBarcodeFormats(Barcode.FORMAT_QR_CODE).build()
 
-        val scanner = BarcodeScanning.getClient()
+        val scanner = BarcodeScanning.getClient(options)
         scanner.process(inputImage)
             .addOnSuccessListener { barcodes ->
                 val strBarcodes = mutableListOf<Barcode>()
