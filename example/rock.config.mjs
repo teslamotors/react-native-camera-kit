186d9c96ce44124d32d9f7d40496968c29e2762d
Michał Pierzchała <thymikee@gmail.com>
2025-09-11 16:10:57 +0300

    setup remote cache

--- example/rock.config.mjs
+++ example/rock.config.mjs
@@ -2,6 +2,7 @@
 import { platformIOS } from '@rock-js/platform-ios';
 import { platformAndroid } from '@rock-js/platform-android';
 import { pluginMetro } from '@rock-js/plugin-metro';
+import { providerGitHub } from '@rock-js/provider-github';
 
 /** @type {import('rock').Config} */
 export default {
@@ -10,5 +11,9 @@ export default {
     ios: platformIOS(),
     android: platformAndroid(),
   },
-  remoteCacheProvider: null,
+  remoteCacheProvider: providerGitHub({
+    owner: 'teslamotors',
+    repository: 'react-native-camera-kit',
+    token: process.env.GITHUB_TOKEN,
+  }),
 };
